fbf5a4069d6f90f5f76f32d9209d04290e93bf30
Abimbola Olaitan <craftwordltd@gmail.com>
2017-02-14 12:16:52 -0800

    adding the httpApi folder which contains all http Api files

---
+++ httpApi/httpPatch.js
@@ -0,0 +1,20 @@
+
+
+var Client = require('node-rest-client').Client;
+ 
+var client = new Client();
+ 
+// set content-type header and data as json in args parameter 
+var args = {
+    data: {
+    title: 'PYTHON AND NODEJS'
+  }
+    headers: { "Content-Type": "application/json" }
+};
+ 
+client.patch("http://jsonplaceholder.typicode.com/posts/1", args, function (data, response) {
+    // parsed response body as js object 
+    console.log(data);
+    // raw response 
+    //console.log(response);
+});
